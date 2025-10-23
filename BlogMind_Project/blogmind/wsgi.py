79f4612f9aa47d1895c56f3b7440919f1e91893c
ksatwikreddy <111113035+ksatwikreddy@users.noreply.github.com>
2025-10-23 22:01:44 +0530

    Delete BlogMind_Project directory

--- BlogMind_Project/blogmind/wsgi.py
+++
@@ -1,8 +0,0 @@
-# blogmind/wsgi.py
-
-import os
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogmind.settings')
-
-application = get_wsgi_application()
